36cc585c38a1fef8fdc3f77076868cd78cf896ef
Gagan Kadadevarmath <gagan2@illinois.edu>
2020-10-16 02:29:46 -0500

    Basic implementation of flashing 'GO' and 'STOP' with no score

---
+++ green-light-red-light/green_light_red_light.py
@@ -0,0 +1,143 @@
+import pygame
+import random
+from itertools import cycle
+from runner import Runner
+import lights
+
+from pygame.locals import (
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREEN = (34,139,34)
+BLUE = (135,235,250)
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 780
+
+GREEN_EVENT = pygame.USEREVENT + 0
+RED_EVENT = pygame.USEREVENT + 1
+
+def red(font):
+    red_light = font.render('GO', True, pygame.Color('green3'))
+    blink_rect = red_light.get_rect()
+    blink_rect.center = screen_rect.center
+    off = pygame.Surface(blink_rect.size)
+    blink_surfaces = cycle([red_light, off])
+    blink_surface = next(blink_surfaces)
+        
+
+pygame.init()
+font = pygame.font.Font('freesansbold.ttf', 100) 
+
+# Create screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen_rect = screen.get_rect()
+
+# Create a runner
+runner = Runner(BLACK, 75, 75)
+
+# # Sprite groups
+# all_sprites = pygame.sprite.Group()
+# all_sprites.add(runner)
+
+# Main function
+def main():
+    running = True
+
+    # Alternates between GO and STOP
+    clock = pygame.time.Clock()
+    # # GO
+    # green_light = font.render('GO', True, pygame.Color('green3'))
+    # green_rect = green_light.get_rect()
+    # green_rect.center = screen_rect.center
+    # green_off = pygame.Surface(green_rect.size)
+    # green_off.fill(BLUE)
+    # green_surfaces = cycle([green_light, green_off])
+    # green_surface = next(green_surfaces)
+    # #pygame.time.set_timer(BLINK_EVENT, random.randint(500,2001))
+
+    # # STOP
+    # red_light = font.render('STOP', True, pygame.Color('red3'))
+    # red_rect = red_light.get_rect()
+    # red_rect.center = screen_rect.center
+    # red_off = pygame.Surface(red_rect.size)
+    # red_off.fill(BLUE)
+    # red_surfaces = cycle([red_light, red_off])
+    # red_surface = next(red_surfaces)
+    # pygame.time.set_timer(BLINK_EVENT, random.randint(500,2001))
+
+    green = lights.Green()
+    red = lights.Red()
+    if random.randint(0, 100) < 40:
+        pygame.time.set_timer(RED_EVENT, random.randint(500,2001))
+    else:
+        pygame.time.set_timer(GREEN_EVENT, random.randint(500,2001))
+
+    count = random.randint(1,2)
+    
+
+    # Main loop
+    while running:
+        # Look for each event in the queue
+        for event in pygame.event.get():
+            # Check if user hit a key
+            if event.type == KEYDOWN:
+                #If esc, QUIT
+                if event.key == K_ESCAPE:
+                    running = False
+                # If window close button, QUIT
+                elif event.key == QUIT:
+                    running = False
+            if event.type == GREEN_EVENT:
+                count = random.randint(0,2)
+                #green_surface = next(green_surfaces)
+                green.green_surface = next(green.green_surfaces)
+                #red_surface = next(red_surfaces)
+                pygame.time.set_timer(GREEN_EVENT, random.randint(500,2001))
+            elif event.type == RED_EVENT:
+                count = random.randint(0,2)
+                #green_surface = next(green_surfaces)
+                red.red_surface = next(red.red_surfaces)
+                #red_surface = next(red_surfaces)
+                pygame.time.set_timer(RED_EVENT, random.randint(500,2001))
+        
+        # Get set of pressed keys
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_RIGHT]:
+            runner.moveRight(5)
+        if pressed_keys[K_LEFT]:
+            runner.moveRight(-5)
+            
+        runner.rect.clamp_ip(screen_rect) 
+
+
+        screen.fill(BLUE)
+
+        # Adds stationary floor
+        floor_surf = pygame.Surface((SCREEN_WIDTH, 50))
+        floor_surf.fill(GREEN)
+        screen.blit(floor_surf, (0, SCREEN_HEIGHT - 50))
+
+        # Blinks green/red
+        # screen.blit(green_surface, green_rect)
+        # screen.blit(red_surface, red_rect)
+        if count == 0:
+            screen.blit(green.green_surface, green.green_rect)
+        elif count == 1:
+            screen.blit(red.red_surface, red.red_rect)
+
+        # Draw sprites
+        screen.blit(runner.surf, runner.rect)
+
+        # Flip everything to display
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
