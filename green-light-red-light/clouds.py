e31d7c4d28873a2dca4cf6e202167faaf6baca1e
Gagan Kadadevarmath <gagan2@illinois.edu>
2020-10-24 23:52:37 -0500

    Added Cloud class

---
+++ green-light-red-light/clouds.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+from pygame.locals import RLEACCEL
+import os
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+SCREEN_WIDTH, SCREEN_HEIGHT = 1024, 800
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load('cloud1.jpg').convert()
+        self.surf = pygame.transform.scale(self.surf, (80, 50))
+        self.surf.set_colorkey(BLACK, RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(SCREEN_WIDTH + 30, SCREEN_WIDTH + 150),
+                random.randint(0, SCREEN_HEIGHT - 69),
+            )
+        )
+    
+    def update(self):
+        self.rect.move_ip(-5, 0)
+        if self.rect.right < 0:
+            self.kill()
