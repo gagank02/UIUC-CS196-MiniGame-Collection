4c9f5281c0d2ddbc37ae1c08b002fe8d4b94b6a3
vaasay <vfarid2@illinois.edu>
2020-11-16 01:27:04 -0600

    Implemented graphics and health bar

--- space-shooter/space_shooter.py
+++ space-shooter/space_shooter.py
@@ -1,15 +1,10 @@
 import pygame
-from sprites import Movement, Enemies
+from sprites import Movement, Enemies, Bullet
+from os import path 
 
 pygame.init()
 pygame.mixer.init()
 clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group()
-movement = Movement()
-all_sprites.add(movement)
-for x in range(10):
-    e = Enemies()
-    all_sprites.add(e)
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -22,25 +17,80 @@ height = 700
 fps = 30
 screen = pygame.display.set_mode((width,height))
 
-pygame.display.set_caption("space shooter")
+pygame.display.set_caption("space shooter") 
 
 running = True
 
+images = path.join(path.dirname(__file__), "images")
+player_image = pygame.image.load(path.join(images, "playerShip3_blue.png")).convert()
+enemy_image = pygame.image.load(path.join(images, "meteorGrey_small1.png")).convert()
+bullet_image = pygame.image.load(path.join(images, "laserRed02.png")).convert()
+
+all_sprites = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+movement = Movement()
+movement.shield = 100
+movement.image = player_image
+movement.image.set_colorkey(black)
+all_sprites.add(movement)
+for x in range(10):
+    e = Enemies()
+    e.image = enemy_image
+    e.image.set_colorkey(black)
+    all_sprites.add(e)
+    enemies.add(e)
+
+def health_bar(surface, x, y, health):
+    if health < 0:
+        health = 0
+    bar_length = 100
+    bar_height = 10
+    current_health = int((health / 100) * bar_length)
+    outer = pygame.Rect(x, y, bar_length, bar_height)
+    current = pygame.Rect(x, y, current_health, bar_height)
+    pygame.draw.rect(surface, green, current)
+    pygame.draw.rect(surface, white, outer, 2)
+
+
 while running:
     clock.tick(30)
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                running = False             
+                running = False
+            if event.key == pygame.K_SPACE:
+                bullet = Bullet(movement.rect.centerx, movement.rect.top)
+                bullet.image = bullet_image
+                bullet.image.set_colorkey(black)
+                all_sprites.add(bullet)
+                bullets.add(bullet)             
 
         if event.type == pygame.QUIT:
             running = False
 
+
     all_sprites.update()
+
+    collisions = pygame.sprite.groupcollide(enemies, bullets, True, True)
+    for i in collisions:
+        e = Enemies()
+        all_sprites.add(e)
+        enemies.add(e)
+
+    collision = pygame.sprite.spritecollide(movement, enemies, True)
+    for i in collision:
+        movement.shield -= 30
+        if movement.shield <= 0:
+            running = False
 
     screen.fill(black)
     
     all_sprites.draw(screen)
+
+    health_bar(screen, 5, 5, movement.shield)
+
     pygame.display.flip()
 
 pygame.quit()
--- space-shooter/sprites.py
+++ space-shooter/sprites.py
@@ -29,17 +29,32 @@ class Enemies(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30,40))
-        self.image.fill((255,0,0))
+        # self.image.fill((255,0,0))
         self.rect = self.image.get_rect()
         self.rect.x =  random.randrange(600 - self.rect.width)
-        self.rect.y = random.randrange(-100, -50)
-        self.y_speed = random.randrange(1,10)
-        self.x_speed = random.randrange(-5,5)
+        self.rect.y = random.randrange(-100, -40)
+        self.y_speed = random.randrange(1,8)
+        self.x_speed = random.randrange(-3,3)
 
     def update(self):
         self.rect.y += self.y_speed
         self.rect.x += self.x_speed
-        if self.rect.top == 700 + 10 or self.rect.left == -10 or self.rect.right == 600 + 10:
+        if self.rect.top > 700 + 10 or self.rect.left < -25 or self.rect.right > 600 + 20:
             self.rect.x =  random.randrange(600 - self.rect.width)
-            self.rect.y = random.randrange(-100,-50)
-            self.y_speed = random.randrange(1,10)
+            self.rect.y = random.randrange(-100,-40)
+            self.y_speed = random.randrange(1,8)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10,20))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.y_speed = -10
+
+    def update(self):
+        self.rect.y += self.y_speed
+        if self.rect.bottom < 0:
+            self.kill()
