2dff9de5aae4598ed7d9ff70954966b7162b42dc
fvial2 <fvial2@illinois.edu>
2020-11-19 13:04:17 -0800

    Finished card class (see code for descriptions of how it functions)

--- card/card.py
+++ card/card.py
@@ -1,8 +1,7 @@
-# import pygame
+import pygame
 from random import randint
 
-# class Card(pygame.sprite.Sprite):
-class Card:
+class Card(pygame.sprite.Sprite):
 
     """
     Class Card
@@ -13,23 +12,24 @@ class Card:
         iw (int): How wide your card is (in px)
     """
     
-    def __init__(self): #, image, ih, iw):
-        """
+    def __init__(self, image, ih, iw):
         super(Card, self).__init__()
         
         self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image, (iw, ih))
         self.rect = self.image.get_rect()
-        """
         
+        # Sets stats to random values between -5 and 5
         self.hm = randint(-5, 5)
         self.mm = randint(-5, 5)
         self.lm = randint(-5, 5)
         self.am = randint(-5, 5)
-        
         self.__randomizer()
         
-        self.name = self.__name_gen()
+        # Initializes the title of the card and its description
+        self.name = " the "
+        self.desc = ""
+        self.__name_desc()
         
     # Corrects any invalid stat totals (all variables must add to 0)
     def __randomizer(self):
@@ -60,28 +60,66 @@ class Card:
                         self.am += 1
                     args[random] += 1
                     total += 1
-        
-    def __name_gen(self):
+    
+    # Sets the title of the card and its description
+    def __name_desc(self):
         args = [self.hm, self.mm, self.lm, self.am]
-        name = " the "
         args.sort(reverse = True)
-        if args[0] > 3:
+        if args[0] == 0:
+            self.name += "Balanced"
+            self.desc += "All stats equal"
+        elif args[0] > 3:
             if args[0] == self.hm:
-                name += "Steadfast"
+                self.name += "Steadfast"
+                self.desc += "Substantial increase in health"
             if args[0] == self.mm:
-                name += "Swift"
+                self.name += "Swift"
+                self.desc += "Substantial increase in speed"
             if args[0] == self.lm:
-                name += "Favored"
+                self.name += "Favored"
+                self.desc += "Substantial increase in luck"
             if args[0] == self.am:
-                name += "Merciless"
+                self.name += "Merciless"
+                self.desc += "Substantial increase in attack"
         else:
             if args[0] == self.hm:
-                if args[1] == self.mm
-                name += "Steadfast"
+                if args[1] == self.mm:
+                    self.name += "Resilient"
+                    self.desc += "Moderate increase in health and speed"
+                if args[1] == self.lm:
+                    self.name += "Gifted"
+                    self.desc += "Moderate increase in health and luck"
+                if args[1] == self.am:
+                    self.name += "Barbarous"
+                    self.desc += "Moderate increase in health and attack"
             if args[0] == self.mm:
-                name += "Swift"
+                if args[1] == self.hm:
+                    self.name += "Resilient"
+                    self.desc += "Moderate increase in health and speed"
+                if args[1] == self.lm:
+                    self.name += "Nimble"
+                    self.desc += "Moderate increase in speed and luck"
+                if args[1] == self.am:
+                    self.name += "Rutheless"
+                    self.desc += "Moderate increase in speed and attack"
             if args[0] == self.lm:
-                name += "Favored"
+                if args[1] == self.hm:
+                    self.name += "Gifted"
+                    self.desc += "Moderate increase in health and luck"
+                if args[1] == self.mm:
+                    self.name += "Nimble"
+                    self.desc += "Moderate increase in speed and luck"
+                if args[1] == self.am:
+                    self.name += "Calculated"
+                    self.desc += "Moderate increase in luck and attack"
             if args[0] == self.am:
-                name += "Merciless"
+                if args[1] == self.hm:
+                    self.name += "Barbarous"
+                    self.desc += "Moderate increase in health and attack"
+                if args[1] == self.mm:
+                    self.name += "Ruthless"
+                    self.desc += "Moderate increase in speed and attack"
+                if args[1] == self.lm:
+                    self.name += "Calculated"
+                    self.desc += "Moderate increase in luck and attack"
         
