6d944625efc2301b93e76ea4fc83ef8d23cef612
Gagan Kadadevarmath <gagan2@illinois.edu>
2020-11-21 19:27:31 -0600

    Added background

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from greenLightRedLight.clouds import Cloud
 pygame.init()
 pygame.display.set_caption('Main Menu')
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 650
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 640
 
 font = pygame.font.Font('freesansbold.ttf', 48)
 button_font = pygame.font.Font('freesansbold.ttf', 16)
@@ -18,6 +18,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
 is_running = True
+
+# Background image
+bg = pygame.image.load("bg.png")
+
+# Colors
+button_clr = (130, 135, 143)
 
 # Buttons
 glrl_button = pygame.Rect(((SCREEN_WIDTH // 2) - 100, 100), (200, 50))
@@ -36,16 +42,17 @@ def main_menu():
 
     click = False
 
-    draw_text("Main Menu", font, (255, 255, 255), screen, 125, 25)
-
     while True:
+        
         background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        background.fill(pygame.Color('#000000'))
+        # background.fill(pygame.Color('#000000'))
+        screen.blit(bg, (0, 0))
+        draw_text("Main Menu", font, (255, 255, 255), screen, 105, 25)
         
         # Mouse coordinates
         mx, my = pygame.mouse.get_pos()
         
-        # Launches specific game based on input
+        # Launches specific game based on mouse input
         if glrl_button.collidepoint((mx,my)):
             if click:
                 pygame.display.set_mode((1024, 800))
@@ -54,21 +61,33 @@ def main_menu():
         #     if click:
         #         pygame.display.set_mode((1280, 720))
         #         boss_rush_main()
+        # if falling_objects_button.collidepoint((mx,my)):
+        #     if click:
+        #         pygame.display.set_mode((1000, 500))
+        #         GLRL_main()
+        # if pong_button.collidepoint((mx,my)):
+        #     if click:
+        #         pygame.display.set_mode((700, 500))
+        #         GLRL_main()
+        # if space_shooter_button.collidepoint((mx,my)):
+        #     if click:
+        #         pygame.display.set_mode((600, 700))
+        #         GLRL_main()
         
         # Draw buttons
-        pygame.draw.rect(screen, (255,0,0), glrl_button)
+        pygame.draw.rect(screen, button_clr, glrl_button)
         screen.blit(button_font.render("Green Light Red Light", True, (255,255,255)), ((SCREEN_WIDTH // 2) - 90, 110))
 
-        pygame.draw.rect(screen, (255,0,0), boss_rush_button)
+        pygame.draw.rect(screen, button_clr, boss_rush_button)
         screen.blit(button_font.render("Boss Rush", True, (255,255,255)), ((SCREEN_WIDTH // 2) - 90, 210))
 
-        pygame.draw.rect(screen, (255,0,0), falling_objects_button)
+        pygame.draw.rect(screen, button_clr, falling_objects_button)
         screen.blit(button_font.render("Falling Objects", True, (255,255,255)), ((SCREEN_WIDTH // 2) - 90, 310))
 
-        pygame.draw.rect(screen, (255,0,0), pong_button)
+        pygame.draw.rect(screen, button_clr, pong_button)
         screen.blit(button_font.render("Pong", True, (255,255,255)), ((SCREEN_WIDTH // 2) - 90, 410))
 
-        pygame.draw.rect(screen, (255,0,0), space_shooter_button)
+        pygame.draw.rect(screen, button_clr, space_shooter_button)
         screen.blit(button_font.render("Space Shooter", True, (255,255,255)), ((SCREEN_WIDTH // 2) - 90, 510))
 
         click = False
